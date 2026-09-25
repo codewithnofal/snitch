a268f01d0f5e9c035baff7b50d28b7f18dd69020
codewithnofal <nofilkachhot@gmail.com>
2026-09-25 22:41:14 +0530

    Add authorization middleware to restrict product route access to sellers only

--- server/src/routes/product.routes.js
+++ server/src/routes/product.routes.js
@@ -1,6 +1,17 @@
 import {Router} from 'express'
+import { authanticate } from '../middlewares/auth.middleware.js';
 
 const router = Router()
+
+router.post('/',authanticate, (req, res, next) => {
+    if(req.user.role !== "seller"){
+        return res.status(403).json({
+            message: "Unauthorized or forbidden"
+        })
+    }
+
+    next()
+})
 
 
 export default router;
